dd880a07fe424bfd20902c539a773285745a2838
ines sakesli <sakesliinesmlc@gmail.com>
2026-01-19 23:34:46 +0100

    Rectif path

--- src/test/java/com/e2eTest/automation/RunWebSuiteTest.java
+++ src/test/java/com/e2eTest/automation/RunWebSuiteTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
     features = {"src/spec/features"},
-    plugin = {"pretty", "json:target/cucumber.json"}, // juste JSON
+    plugin = {"pretty", "json:target/report/cucumber.json"}, // juste JSON
     tags = ("@login"),
     monochrome = true,
     snippets = CAMELCASE
